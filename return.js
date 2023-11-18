32abea18a0851659f3fedcc075fe1e4d4d2119c1
unknown <biswasmahmuda5@gmail.com>
2023-11-17 16:36:45 -0800

    create function and get many parameters

---
+++ return.js
@@ -0,0 +1,16 @@
+const totalNumber = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+};
+
+const result = totalNumber(20, 20);
+console.log(result);
+
+const bringSingra = (money) => {
+  const singraPrize = 10;
+  const totalSingra = money / singraPrize;
+  return totalSingra;
+};
+
+const results = bringSingra(150);
+console.log(results);
